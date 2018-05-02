c669a1499656c8db3b135225b97720472c588ec1
Taras Shpachenko <taras.shpachenko@onapp.com>
2018-05-02 13:05:49 +0300

    Add test coverage.

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
-[![Heroku](http://heroku-badge.herokuapp.com/?app=angularjs-crypto&svg=1)](https://new-alter.herokuapp.com/)
+[![Heroku](http://heroku-badge.herokuapp.com/?app=new-alter&svg=1)](https://new-alter.herokuapp.com/)
 [![Build Status](http://circleci-badges-max.herokuapp.com/img/floor114/new_alter/master?token=a2cb9700cd102f5ec3f87ee42c3047ce815a20de)](https://circleci.com/gh/floor114/new_alter/tree/master)
+[![codecov](https://codecov.io/gh/floor114/new_alter/branch/master/graph/badge.svg?token=kJV4qlokrm)](https://codecov.io/gh/floor114/new_alter)
 
 
 # NewAlter
---
+++ spec/simplecov_helper.rb
@@ -0,0 +1,42 @@
+require 'simplecov'
+
+# save to CircleCI's artifacts directory if we're on CircleCI
+if ENV['CIRCLE_ARTIFACTS']
+  dir = File.join(ENV['CIRCLE_ARTIFACTS'], 'coverage')
+  SimpleCov.coverage_dir(dir)
+end
+
+SimpleCov.start do
+  add_filter '/spec'
+  add_filter '/config'
+  add_filter '/lib/tasks'
+  add_filter '/channel'
+  add_filter '/controllers'
+
+  # TODO: Uncomment and delete row below when feature tests will be present
+  add_filter 'render_helper'
+  add_filter '/cell'
+  # add_group 'Cells' do |src_file|
+  #   src_file.project_filename.match(%r{\/cell})
+  # end
+
+  add_group 'Contracts' do |src_file|
+    src_file.project_filename.match(%r{\/contract})
+  end
+
+  add_group 'Operations' do |src_file|
+    src_file.project_filename.match(%r{\/operation})
+  end
+
+  add_group 'Models', 'app/models'
+  add_group 'Helpers', 'app/helpers'
+  add_group 'Services' do |src_file|
+    src_file.project_filename.match(%r{\/services\/})
+  end
+
+  add_group 'Policies', 'app/policies'
+end
+
+require 'codecov'
+
+SimpleCov.formatter = SimpleCov::Formatter::Codecov
